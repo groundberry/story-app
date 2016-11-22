496c5eaee17c4d76908640d6e109977845b79da1
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-22 11:45:23 +0000

    Amended arrays and functions to meet MVP lvl 1

--- src/Story.js
+++ src/Story.js
@@ -1,24 +1,14 @@
 Story = function() {
-  this.sentencesArray = ["A", "sat on the"];
   this.subjectArray = ["cat", "dog", "donkey"];
   this.objectArray = ["toilet", "sofa", "mat"];
-  this.completeSentence = [];
 };
 
-Story.prototype.sentencePicker = function () {
-  this.completeSentence.push(this.sentencesArray);
-  console.log(this.completeSentence[0]);
-};
-
-
-Story.prototype.subjectPicker = function () {
-  this.completeSentence[0].splice(1, 0, "dog");
-  console.log(this.completeSentence);
+Story.prototype.subjectPicker = function (index) {
+  var subject = this.subjectArray[index];
+  return subject;
 };
 
 Story.prototype.objectPicker = function (index) {
-  this.completeSentence[0].push(this.objectArray[index+1]);
-  console.log(this.completeSentence);
-  var test = [].concat.apply([], this.completeSentence);
-  console.log(test.join());
+  var object = this.objectArray[index];
+  return object;
 };
