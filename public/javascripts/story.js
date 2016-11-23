ee6013818440096122d59fac1b84e852323bf22d
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-23 09:38:54 +0000

    fixed links

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,37 +1,11 @@
 Story = function() {
-  this.subjectArray = ["cat", "dog", "donkey"];
-  this.objectArray = ["toilet", "sofa", "mat"];
-  this.sentencesArray = ["A", "sat on the"];
-  this.completeSentence = [];
-};
+  this.animalArray = ["cat", "dog", "donkey", "monkey", "zebra"];
+  this.placesArray = ["toilet", "sofa", "mat", "throne", "roof"];
 
-Story.prototype.subjectPicker = function (index) {
-  var subject = this.subjectArray[index];
-  return subject;
-};
-
-Story.prototype.objectPicker = function (index) {
-  var object = this.objectArray[index];
-  return object;
-};
-
-Story.prototype.subjectSelector = function (index) {
-  var item = this.subjectArray[index];
-  this.sentencesArray.splice(1, 0, item);
-};
-
-Story.prototype.objectSelector = function (index) {
-  this.sentencesArray.push(this.objectArray[index]);
-  this.completeSentence.push(this.sentencesArray.join(" "));
-  console.log(this.completeSentence);
-};
-
-Story.prototype.consLog = function() {
-  console.log(this.sentencesArray);
 };
 
 Story.prototype.animalLinkCreator = function () {
-  this.subjectArray.forEach(function(animal) {
+  this.animalArray.forEach(function(animal) {
     var newLi = document.createElement("li");
     newLi.innerHTML = "<a class='animal' href=#>" + animal + "</a>";
     console.log(newLi);
@@ -40,11 +14,11 @@ Story.prototype.animalLinkCreator = function () {
 };
 
 Story.prototype.placeLinkCreator = function () {
-  this.objectArray.forEach(function(place) {
-    var newLi = document.createElement("li");
-    newLi.innerHTML = "<a class='places' href=#>" + place + "</a>";
-    console.log(newLi);
-    document.getElementById("places").appendChild(newLi);
+  this.placesArray.forEach(function(place) {
+    var newLi1 = document.createElement("li");
+    newLi1.innerHTML = "<a class='place' href=#>" + place + "</a>";
+    console.log(newLi1);
+    document.getElementById("places").appendChild(newLi1);
 });
 };
 
