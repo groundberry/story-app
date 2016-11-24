529ce8dbce0792122797d63a7f32c65984d85a23
your name <akram.rasikh@gmail.com>
2016-11-24 16:28:05 +0000

    working on moveClick test

---
+++ public/javascripts/featureSeeWordSpec.js
@@ -0,0 +1,53 @@
+
+function seeWord() {
+  if(document.getElementById('animal1') === null) {
+    console.log("Feature test: Test failed");
+  } else {
+    console.log("Feature test: Test passed");
+  }
+}
+
+
+function listContainsAnimal() {
+  var word = document.getElementById('animal1').innerHTML;
+  var finding = word.includes("lion");
+  if (finding === true) {
+    console.log("Feature test: Test passed");
+  } else {
+    console.log("Feature test: Test failed");
+  }
+}
+
+function spanCheck() {
+  if (document.getElementsByTagName('word0').length === 0) {
+    console.log("Feature test: Test passed");
+  } else {
+    console.log("Feature test: Test failed");
+  }
+}
+
+
+function clickMove() {
+  bert = document.querySelectorAll('#name1 > li:nth-child(1) > a')[0].innerHTML;
+  console.log(bert);
+}
+
+
+function checkWord() {
+  var cells = document.getElementsByTagName("li");
+  var len = cells.length;
+  var str = "";
+  for (var i=0; i<len; i++) {
+    if (cells.id !== null){
+      console.log("Feature test: Test passed");
+    } else {
+      console.log("Feature test: Test failed");
+    }
+  }
+}
+
+  seeWord();
+  checkWord();
+  listContainsAnimal();
+  spanCheck();
+  clickMove();
--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,26 +1,26 @@
-  var items = {
-      name1: ["Bert", "Rupert", "Daisy", "Ruby", "Timmy"],
-      animal1: ["cat", "dog", "lion", "otter", "pig"],
-      toys: ["ball", "catapult", "cars", "bikes", "frisbees"],
-      chores: ["brushing their teeth", "tidying their room", "going shopping", "having a bath", "going to sleep"],
-      places: ["toilet", "sofa", "mat", "throne", "roof"],
-      food: ["pizza", "chocolate", "carrots", "chips", "lettuce"],
-      name2: ["Bob", "Steve", "Jimmy", "Anne", "Katy"],
-      animal2: ["monkey", "zebra", "duck", "cow", "penguin"],
-      game: ["tag", "hide and seek", "sleeping lions", "hopscotch", "what's the time Mr Wolf"],
-      feelings: ["happy", "excited", "sleepy", "bored", "sad"]
-    };
+var items = {
+  name1: ["Bert", "Rupert", "Daisy", "Ruby", "Timmy"],
+  animal1: ["cat", "dog", "lion", "otter", "pig"],
+  toys: ["ball", "catapult", "cars", "bikes", "frisbees"],
+  chores: ["brushing their teeth", "tidying their room", "going shopping", "having a bath", "going to sleep"],
+  places: ["toilet", "sofa", "mat", "throne", "roof"],
+  food: ["pizza", "chocolate", "carrots", "chips", "lettuce"],
+  name2: ["Bob", "Steve", "Jimmy", "Anne", "Katy"],
+  animal2: ["monkey", "zebra", "duck", "cow", "penguin"],
+  game: ["tag", "hide and seek", "sleeping lions", "hopscotch", "what's the time Mr Wolf"],
+  feelings: ["happy", "excited", "sleepy", "bored", "sad"]
+};
 
 
-  var linkCreator = function () {
-    Object.keys(items).forEach(function(key) {
-      var arr = items[key];
-      arr.forEach(function(item) {
-        var link = document.createElement("li");
-        link.innerHTML = "<a class='" + key + "' href='#'>" + item + "</a>";
-        document.getElementById(key).appendChild(link);
-      });
+var linkCreator = function () {
+  Object.keys(items).forEach(function(key) {
+    var arr = items[key];
+    arr.forEach(function(item) {
+      var link = document.createElement("li");
+      link.innerHTML = "<a class='" + key + "' href='#'>" + item + "</a>";
+      document.getElementById(key).appendChild(link);
     });
-  };
+  });
+};
 
-  linkCreator();
+linkCreator();
