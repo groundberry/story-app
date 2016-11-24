82717d50318eeb3300053e03a26aa7dcebf6dc1f
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-11-24 19:30:06 +0000

    Show only one list of words on screen at a time

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -16,51 +16,51 @@ $(document).ready(function () {
   });
 
 
+  //
+  //
+  // $('.name1').on('click', function () {
+  //   $('#word0').replaceWith(this);
+  // });
+  //
+  // $('.animal1').on('click', function () {
+  //   $('#word1').replaceWith(this);
+  // });
+  //
+  // $('.toys').on('click', function () {
+  //   $('#word2').replaceWith(this);
+  // });
+  //
+  // $('.chores').on('click', function () {
+  //   $('#word3').replaceWith(this);
+  // });
+  //
+  // $('.places').on('click', function () {
+  //   $('#word4').replaceWith(this);
+  // });
+  //
+  // $('.food').on('click', function () {
+  //   $('#word5').replaceWith(this);
+  // });
+  //
+  // $('.name2').on('click', function () {
+  //   $('#word6').replaceWith(this);
+  // });
+  //
+  // $('.animal2').on('click', function () {
+  //   $('#word7').replaceWith(this);
+  // });
+  //
+  // $('.game').on('click', function () {
+  //   $('#word8').replaceWith(this);
+  // });
+  //
+  // $('.feelings').on('click', function () {
+  //   $('#word9').replaceWith(this);
+  // });
 
-
-  $('.name1').on('click', function () {
-    $('#word0').replaceWith(this);
-  });
-
-  $('.animal1').on('click', function () {
-    $('#word1').replaceWith(this);
-  });
-
-  $('.toys').on('click', function () {
-    $('#word2').replaceWith(this);
-  });
-
-  $('.chores').on('click', function () {
-    $('#word3').replaceWith(this);
-  });
-
-  $('.places').on('click', function () {
-    $('#word4').replaceWith(this);
-  });
-
-  $('.food').on('click', function () {
-    $('#word5').replaceWith(this);
-  });
-
-  $('.name2').on('click', function () {
-    $('#word6').replaceWith(this);
-  });
-
-  $('.animal2').on('click', function () {
-    $('#word7').replaceWith(this);
-  });
-
-  $('.game').on('click', function () {
-    $('#word8').replaceWith(this);
-  });
-
-  $('.feelings').on('click', function () {
-    $('#word9').replaceWith(this);
-  });
-
-  $('.button').on('click', function () {
-    getSentence();
-  });
+  // $('.button').on('click', function () {
+  //   getSentence();
+  // });
 
   getSentence();
 });
--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -1,41 +1,47 @@
-  var items = {
-      name1: ["Bert", "Rupert", "Daisy", "Ruby", "Timmy"],
-      animal1: ["cat", "dog", "lion", "otter", "pig"],
-      toys: ["ball", "catapult", "cars", "bikes", "frisbees"],
-      chores: ["brushing their teeth", "tidying their room", "going shopping", "having a bath", "going to sleep"],
-      places: ["toilet", "sofa", "mat", "throne", "roof"],
-      food: ["pizza", "chocolate", "carrots", "chips", "lettuce"],
-      name2: ["Bob", "Steve", "Jimmy", "Anne", "Katy"],
-      animal2: ["monkey", "zebra", "duck", "cow", "penguin"],
-      game: ["tag", "hide and seek", "sleeping lions", "hopscotch", "what's the time Mr Wolf"],
-      feelings: ["happy", "excited", "sleepy", "bored", "sad"]
-    };
-
-
-  var linkCreator = function () {
-    Object.keys(items).forEach(function(key) {
-      var arr = items[key];
-      arr.forEach(function(item) {
-        var link = document.createElement("li");
-        link.innerHTML = "<a class='" + key + "' href='#'>" + item + "</a>";
-        document.getElementById(key).appendChild(link);
-      });
-    });
+var items = {
+    'name': ["Bert", "Rupert", "Daisy", "Ruby", "Timmy"],
+    'animals': ["cat", "dog", "lion", "otter", "pig"],
+    'toys': ["ball", "catapult", "cars", "bikes", "frisbees"],
+    'chores': ["brushing their teeth", "tidying their room", "going shopping", "having a bath", "going to sleep"],
+    'places': ["toilet", "sofa", "mat", "throne", "roof"],
+    'food': ["pizza", "chocolate", "carrots", "chips", "lettuce"],
+    'friend': ["Bob", "Steve", "Jimmy", "Anne", "Katy"],
+    'animal:': ["monkey", "zebra", "duck", "cow", "penguin"],
+    'game': ["tag", "hide and seek", "sleeping lions", "hopscotch", "what's the time Mr Wolf"],
+    'feelings': ["happy", "excited", "sleepy", "bored", "sad"]
   };
 
 
-  function changeText() {
-    console.log(document.getElementsByClassName('name1')[0]);
-    document.getElementById('word0').innerHTML = document.getElementsByClassName('name1')[0].innerHTML.fontcolor('#337ab7');
-  }
+var getTitle = function () {
+  var key = Object.keys(items)[0];
+  document.getElementById("title").innerHTML = key;
+  getWords(key);
+};
+
+var getWords = function (key) {
+  var arrayOfWords = items[key];
+  arrayOfWords.forEach(function(word) {
+    var link = document.createElement("li");
+    link.innerHTML = "<a class=" + key + "' href='#'>" + word + "</a>";
+    document.getElementById('words').appendChild(link);
+  });
+};
 
 
-  window.onload = function() {
-    document.getElementsByClassName('name1')[0].addEventListener('click',function () {
-      changeText();
-    });
-  };
+
+function changeText(key) {
+  document.getElementById('word0').innerHTML = document.getElementsByClassName('Name')[0].innerHTML.fontcolor('#337ab7');
+}
+
+window.onload = function() {
+  document.getElementsByClassName('Name')[0].addEventListener('click',function () {
+    changeText();
+  });
+};
 
 
 
-  linkCreator();
+
+
+getTitle();
+linkCreator();
